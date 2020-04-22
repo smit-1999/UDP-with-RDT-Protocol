b56b5c105a4aff45cc154c616ceca789bca13354
shik-25 <shikhar.dhawale@gmail.com>
2020-04-22 20:47:10 +0530

    added packet class

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
-
+import packet
+import pickle
 # set up the socket using local address
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket.bind(("", 9999))
@@ -21,11 +22,12 @@ while connection != True:
 while 1:
 
     # get the data sent to us
-    data, ip = socket.recvfrom(1024)
-
+    #data, ip = socket.recvfrom(1024)
+    data = socket.recv(4096)
+    data_variable = pickle.loads(data)
     # display
-    print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
-
+    #print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+    print(data_variable)
     # echo back
     data = input("Server >")
     data = data.encode() 
